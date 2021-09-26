1056fc332a72939df5f5f87da710f838c74847cc
Maxim Savenko <maxim.savenko@gmail.com>
2021-09-26 20:57:41 +0300

    ...

---
+++ Topics/Secondary constructor/Barista problem/src/Main.kt
@@ -0,0 +1,11 @@
+class EspressoMachine {
+    val costPerServing: Float
+
+    constructor(coffeeCapsulesCount: Int, totalCost: Float) {
+        this.costPerServing = totalCost / coffeeCapsulesCount
+    }
+
+    constructor(coffeeBeansWeight: Float, totalCost: Float) {
+        this.costPerServing = totalCost / (coffeeBeansWeight / 10)
+    }
+}
